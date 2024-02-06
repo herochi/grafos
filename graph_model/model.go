4d791ba1211dc03295ca855d5ee6ebd52578ef76
ricardochi <ricardo.chi@kiban.mx>
2024-02-05 18:14:55 -0600

    add delete nodes and add nodes

--- graph_model/model.go
+++ graph_model/model.go
@@ -2,6 +2,11 @@ package graph_model
 
 import "fmt"
 
+/*
+Mi idea de grafo es que sea un grafo ponderado
+la primer key del map representa el nodo, la segunda key del siguiente map representa el nodo con el cual comparte
+una arista y el int final representa el peso
+*/
 type Graph struct {
 	Nodes map[int]map[int]int
 }
@@ -13,9 +18,17 @@ func NewGraph() *Graph {
 }
 
 func (it *Graph) AddEdge(nodeOrigin, nodeDestiny, weight int) {
+	//Si el nodo de origen no existe lo crea
 	if _, ok := it.Nodes[nodeOrigin]; !ok {
 		it.Nodes[nodeOrigin] = make(map[int]int)
 	}
+
+	//Si el nodo de destino no existe lo crea
+	if _, ok := it.Nodes[nodeDestiny]; !ok {
+		it.Nodes[nodeDestiny] = make(map[int]int)
+	}
+
+	//agrega la arista con su peso
 	it.Nodes[nodeOrigin][nodeDestiny] = weight
 }
 
@@ -29,11 +42,18 @@ func (it *Graph) AddNode(node int) bool {
 }
 
 func (it *Graph) PrintGraph() {
+	isFirst := true
 	for node, edges := range it.Nodes {
-		fmt.Printf("Node %d -> ", node)
+		fmt.Printf("Node %d -> [", node)
 		for nodeEdge, weight := range edges {
-			fmt.Printf("( edge [%d, %d], weight: %d) ", node, nodeEdge, weight)
+			if !isFirst {
+				fmt.Printf(", ")
+			}
+			fmt.Printf(" ( edge [%d, %d], weight: %d) ", node, nodeEdge, weight)
+			isFirst = false
 		}
+		fmt.Print("]")
+		isFirst = true
 		fmt.Println()
 	}
 }
@@ -42,6 +62,8 @@ func (it *Graph) DeleteNode(nodeToDelete int) bool {
 	if _, ok := it.Nodes[nodeToDelete]; ok {
 		delete(it.Nodes, nodeToDelete)
 
+		//Al no existir el nodo es importante eliminar de los nodos restantes las aristas que comarten
+		//Ya que estas tampoco existirían
 		for node, edges := range it.Nodes {
 			for nodeEdge, _ := range edges {
 				if nodeEdge == nodeToDelete {
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	//graphExample1()
 	//graphExample2()
 	//graphExample3()
-	graphExample4()
+	//graphExample4()
 }
 
 func graphExample1() {
@@ -21,6 +21,7 @@ func graphExample1() {
 	graph.AddEdge(2, 5, 3)
 	graph.AddEdge(3, 4, 1)
 	graph.AddEdge(4, 5, 7)
+	graph.AddEdge(4, 6, 9)
 
 	graph.PrintGraph()
 }
@@ -56,14 +57,23 @@ func graphExample4() {
 	graph.AddNode(1)
 	graph.AddNode(2)
 	graph.AddNode(33)
+	graph.AddEdge(1, 3, 5)
+	graph.AddEdge(2, 4, 5)
+	graph.AddEdge(2, 5, 3)
+	graph.AddEdge(3, 6, 1)
+	graph.AddEdge(3, 7, 7)
 	graph.PrintGraph()
 
 	fmt.Println("delete node 33")
 	graph.DeleteNode(33)
 	graph.PrintGraph()
 
-	fmt.Println("add edge in node 1 to 2")
+	fmt.Println("add edge in node 1 to 2, with weight 2")
 	graph.AddEdge(1, 2, 2)
+	graph.PrintGraph()
+
+	fmt.Println("add edge in node 3 to 2, with weight 10")
+	graph.AddEdge(3, 2, 10)
 	graph.PrintGraph()
 
 	fmt.Println("delete node 2")
